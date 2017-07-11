de447f5dd6b54ba86aa490872135b837bad13e97
susanchang0806 <s79536117@gmail.com>
2017-07-11 21:35:21 +0800

    0711_afterClass1
    0711_afterClass1

---
+++ 0711example/0711-Example02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentMgtLibrary;
+
+namespace _0711_Example02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student aStudent = new Student()
+            {
+                name = "Susan",
+                phone = "0987656544",
+                address = "Taipei, Taiwan"
+
+            };
+
+            Console.WriteLine("學生姓名:{0}, 電話:{1}, 住址:{2}"
+                    , aStudent.name
+                    , aStudent.phone
+                    , aStudent.address
+                );
+
+        }
+    }
+}
--- 0711example/0711example/Program.cs
+++ 0711example/0711example/Program.cs
@@ -10,6 +10,9 @@ namespace _0711example
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("{0} Hello World {1} years, {2} days, {3} hours.", "Hi", "11", "23", "15");
+
+            Console.WriteLine("AAAAAAAA");
         }
     }
 }
