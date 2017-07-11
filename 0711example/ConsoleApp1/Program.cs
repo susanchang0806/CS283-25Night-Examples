eca2cb0fabc7cdd159df0121a654a51167bd21ca
susanchang0806 <s79536117@gmail.com>
2017-07-11 22:15:11 +0800

    0711Class
    0711Class

---
+++ 0711example/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentMgtLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student aStudent = new Student();
+            Console.Write("請輸入學生姓名");
+            aStudent.name = Console.ReadLine();
+            Console.Write("請輸入學生電話");
+            aStudent.phone = Console.ReadLine();
+            Console.Write("請輸入學生地址");
+            aStudent.address = Console.ReadLine();
+
+            Console.WriteLine("學生姓名:{0}, 電話:{1}, 住址:{2}"
+                , aStudent.name
+                , aStudent.phone
+                , aStudent.address
+            );
+
+        }
+    }
+}
