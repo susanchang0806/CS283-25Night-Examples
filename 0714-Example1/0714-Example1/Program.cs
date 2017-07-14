fdcd4acc1c8c8ff69e83a3377dacbc096a41d362
susanchang0806 <s79536117@gmail.com>
2017-07-14 20:08:29 +0800

    add example v1

---
+++ 0714-Example1/0714-Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0714_Example1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("請輸入數字1:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("請輸入數字2:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}+{1}={2}"
+                , number1
+                , number2
+                , number1 + number2);
+        }
+    }
+}
