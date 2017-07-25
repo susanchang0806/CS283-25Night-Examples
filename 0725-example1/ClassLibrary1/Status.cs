73ed49178372883f2537d005fdfbd39043c21796
susanchang0806 <s79536117@gmail.com>
2017-07-25 21:41:06 +0800

    0725 Example

---
+++ 0725-example1/ClassLibrary1/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 0725_example1
+{
+    public enum Status
+{
+    Success, Error, Discount, Connected
+}
+}
---
+++ 0725-example3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0725_example3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+            int sum = 0;
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                numbers[index] = random.Next(-1000, 1000);
+                string numb = numbers[index] < 0 ? "(" + numbers[index].ToString() + ")" : numbers[index].ToString();
+                sum += numbers[index];
+                if (index == 0)
+                {
+                    Console.WriteLine("{0}", numb);
+                }
+                else
+                {
+                    Console.WriteLine("+{0}", numb);
+                }
+            }
+
+            Console.WriteLine("={0}", sum);
+        }
+    }
+}
